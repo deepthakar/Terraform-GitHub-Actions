ac7266d10f63a108f0a032e1f05afdebc13bdda1
deepthakar <43946394+deepthakar@users.noreply.github.com>
2024-04-25 02:32:48 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,6 +23,6 @@ resource "aws_instance" "test_instance" {
   ami           = "ami-0f496107db66676ff"
   instance_type = "t2.nano"
   tags = {
-    Name = "test_instance1"
+    Name = "state_test_instance1"
   }
 }
