18b536d550bc446c07aebeee1937caef13aebed9
deepthakar <43946394+deepthakar@users.noreply.github.com>
2024-04-25 03:44:52 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket         	   = "terraform-state-logicmonitor"
-    key                = "state/terraform.tfstate"
+    #key                = "state/terraform.tfstate"
     region         	   = "us-east-1"
     encrypt        	   = true
     dynamodb_table     = "mycomponents_tf_lockid"
