d49bf20356a112a8aa954c21f843916eb67fbb3b
deepthakar <43946394+deepthakar@users.noreply.github.com>
2024-04-25 01:33:04 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,14 @@ terraform {
   }
 }
 
-
+terraform {
+  backend "s3" {
+    bucket                  = "terraform-s3-state-0223"
+    key                     = "my-terraform-project"
+    region                  = "us-east-1"
+    #shared_credentials_file = "~/.aws/credentials"
+  }
+}
 
 provider "aws" {
   region = "us-east-1"
