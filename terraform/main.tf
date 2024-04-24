b8e7800492e18e7ef0fb71ebcf38b90bed1ac932
deepthakar <43946394+deepthakar@users.noreply.github.com>
2024-04-25 03:59:40 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "demo" {
+  ami         	 = "ami-0f496107db66676ff"
+  instance_type = "t2.micro"
+}
+
+
+
 terraform {
   backend "s3" {
 	bucket     	= "terraform-state-logicmonitor"
