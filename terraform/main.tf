d47bcd0caa7af5ced07b4380bfbd02e080cbd328
deepthakar <43946394+deepthakar@users.noreply.github.com>
2024-04-25 03:56:46 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,9 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.18.0"
-    }
-  }
-
   backend "s3" {
-    bucket         	   = "terraform-state-logicmonitor"
-    key                = "s3://terraform-state-logicmonitor/terraform.tfstate"
-    region         	   = "us-east-1"
-    encrypt        	   = true
-    dynamodb_table     = "mycomponents_tf_lockid"
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_instance" "test_instance" {
-  ami           = "ami-0f496107db66676ff"
-  instance_type = "t2.nano"
-  tags = {
-    Name = "state_test_instance1"
+	bucket     	= "terraform-state-logicmonitor"
+	key        	= "s3://terraform-state-logicmonitor/statefile.tfstate"
+	region     	= "us-east-1"
+	encrypt    	= true
+	dynamodb_table = "mycomponents_tf_lockid"
   }
 }
