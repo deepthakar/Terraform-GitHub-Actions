70a59b2da5fc634d7543b8c86b5633bb8aedb526
deepthakar <43946394+deepthakar@users.noreply.github.com>
2024-04-25 00:52:54 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket                  = "my-terraform-state-bucket-897"
+    #key                     = "my-terraform-project"
+    region                  = "us-east-1"
+    #shared_credentials_file = "~/.aws/credentials"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "test_instance" {
+  ami           = "ami-0f496107db66676ff"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "test_instance"
+  }
+}
