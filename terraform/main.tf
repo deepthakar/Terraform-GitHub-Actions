575fccb9265eb0e218a87920f4ce77f87f9df58a
deepthakar <43946394+deepthakar@users.noreply.github.com>
2024-04-25 02:05:23 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,7 @@ terraform {
     key               = "state/terraform.tfstate"
     region         	   = "us-east-1"
     encrypt        	   = true
-    #dynamodb_table = "mycomponents_tf_lockid"
+    dynamodb_table = "mycomponents_tf_lockid"
   }
 }
 
