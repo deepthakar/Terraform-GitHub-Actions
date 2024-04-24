c321dfe95e69372bd953d1b34b0b6bbd900bf704
deepthakar <43946394+deepthakar@users.noreply.github.com>
2024-04-25 04:12:06 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_instance" "demo" {
+resource "aws_instance" "demo1" {
   ami         	 = "ami-0f496107db66676ff"
   instance_type = "t2.micro"
 }
