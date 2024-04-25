6579a7e826552ff9495474520339a7345a21076d
deepthakar <43946394+deepthakar@users.noreply.github.com>
2024-04-25 06:19:41 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+# main.tf
+provider "aws" {
+  region = "us-east-1"
+}
+terraform {
+ backend "s3" {
+	bucket     	= "terraform-state-logicmonitor"
+	key        	= "terraform.tfstate"
+	region     	= "us-east-1"
+	encrypt    	= true
+	dynamodb_table = "mycomponents_tf_lockid"
+  }
+}
+resource "aws_instance" "demo" {
+  ami       	 = "ami-0f496107db66676ff"
+  instance_type = "t2.micro"
+  tags = {
+	Name = "DemoEC2Instance"
+  }
+}
